0b1fa4b9346b8d75c35f889bc9bc7ea7567837bb
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-05-30 11:51:35 +0300

    modified app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import "./assets/style/style.css";
+import styled from "styled-components";
+import Search from "./components/Search/Search";
+import Weather from "./components/Weather/Weather";
+
+const AppStyle = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.4);
+  color: #fff;
+
+  &&:before {
+    content: '';
+    background: url('https://raw.githubusercontent.com/fireclint/weather-app-react/main/src/assets/sunset.jpg') no-repeat center center/cover;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  }
+`;
+
 const App = () => {
     return (
-        <div>
-            Hi
-        </div>
+        <AppStyle>
+            <Search/>
+            <Weather/>
+        </AppStyle>
     );
 }
 
